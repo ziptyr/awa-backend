e9dabfc0f1fe8742f737f28f5e31a447d58c7421
ziptyr <koulu@debian.localdomain>
2021-12-18 13:58:33 +0200

    readding sql commands

---
+++ sql/insert-restaurants.sql
@@ -0,0 +1,22 @@
+INSERT INTO restaurants (restaurant_id, restaurant_name, manager_name, address, opens, closes, price_level, type, image) VALUES (
+    1,
+    'best burgers',
+    'lucas',
+    'sargans',
+    '9:00',
+    '22:00',
+    2,
+    'fast food',
+    '/image/path'
+);
+INSERT INTO restaurants (restaurant_id, restaurant_name, manager_name, address, opens, closes, price_level, type, image) VALUES (
+    2,
+    'even better burgers',
+    'lucas',
+    'sargans',
+    '9:00',
+    '22:00',
+    1,
+    'street food',
+    '/image/path'
+);
---
+++ sql/migration-down.sql
@@ -0,0 +1,10 @@
+drop table orders_products;
+drop table products;
+drop table orders;
+drop table restaurants;
+drop table users;
+
+
+drop sequence restaurant_pk_seq;
+drop sequence product_pk_seq;
+drop sequence order_pk_seq;
