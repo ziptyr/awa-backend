a5759b32cbdd5cf906cededd31e6a110a01ec698
ziptyr <koulu@debian.localdomain>
2021-12-18 22:46:13 +0200

    added AccountService

---
+++ src/main/java/com/foodapp/awabackend/service/AccountService.java
@@ -0,0 +1,29 @@
+package com.foodapp.awabackend.service;
+
+import java.util.List;
+
+import com.foodapp.awabackend.data.Account;
+import com.foodapp.awabackend.data.Role;
+import com.foodapp.awabackend.repo.AccountRepo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AccountService {
+
+    @Autowired
+    AccountRepo accountRepo;
+
+    public Account findByUserName(String userName) {
+        return accountRepo.findByUserName(userName);
+    }
+
+    public List<Account> findUsersByRole(Role role) {
+        return accountRepo.findUsersByRole(role);
+    }
+
+    public void save(Account account) {
+        accountRepo.save(account);
+    }
+}
