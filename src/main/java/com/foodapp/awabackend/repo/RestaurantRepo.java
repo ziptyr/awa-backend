4b848d4c83c95e0c06ba1b940aeb6a4e4b998495
ziptyr <koulu@debian.localdomain>
2021-12-18 22:06:23 +0200

    added missing @Repository

--- src/main/java/com/foodapp/awabackend/repo/RestaurantRepo.java
+++ src/main/java/com/foodapp/awabackend/repo/RestaurantRepo.java
@@ -5,7 +5,9 @@ import java.util.List;
 import com.foodapp.awabackend.data.Restaurant;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RestaurantRepo extends JpaRepository<Restaurant, Long> {
 
     public Restaurant findById(long restaurantId);
