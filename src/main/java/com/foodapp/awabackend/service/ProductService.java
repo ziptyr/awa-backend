ac2d2dd52770cc3fe4c768095795f199008cd3c7
ziptyr <koulu@debian.localdomain>
2021-12-19 14:28:53 +0200

    ProductService save()

--- src/main/java/com/foodapp/awabackend/service/ProductService.java
+++ src/main/java/com/foodapp/awabackend/service/ProductService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import com.foodapp.awabackend.data.Product;
+import com.foodapp.awabackend.data.Restaurant;
 import com.foodapp.awabackend.repo.ProductRepo;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,10 +16,18 @@ public class ProductService {
     @Autowired
     ProductRepo productRepo;
 
+    @Autowired
+    RestaurantService restaurantService;
+
     public List<Product> getProducts(List<Long> productIds) {
 
         return productIds.stream()
             .map(productId -> productRepo.findById(productId).orElse(null))
             .collect(Collectors.toList());
+    }
+
+    public Restaurant save(Product product) {
+        productRepo.save(product);
+        return restaurantService.findById(product.getRestaurantId());
     }
 }
