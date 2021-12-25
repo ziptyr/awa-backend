62f2a2c289c6bc8b89cffed1c18be3afb4aab941
ziptyr <>
2021-12-25 22:13:45 +0200

    MySecurityConfiguration CORS

--- src/main/java/com/foodapp/awabackend/AwaBackendController.java
+++ src/main/java/com/foodapp/awabackend/AwaBackendController.java
@@ -31,6 +31,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -76,6 +77,7 @@ public class AwaBackendController {
         accountService.save(account);
     }
 
+    //@CrossOrigin(origins = "http://localhost:8080")
     @GetMapping("/public/restaurants")
     public List<Restaurant> getRestaurants() {
         return restaurantService.findAll();
--- src/main/java/com/foodapp/awabackend/security/conf/MySecurityConfiguration.java
+++ src/main/java/com/foodapp/awabackend/security/conf/MySecurityConfiguration.java
@@ -1,18 +1,23 @@
 package com.foodapp.awabackend.security.conf;
 
+import java.util.Arrays;
+
 import com.foodapp.awabackend.data.Role;
 import com.foodapp.awabackend.security.encoder.MyPasswordEncoder;
 import com.foodapp.awabackend.security.service.UserServiceImplementation;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -33,6 +38,7 @@ public class MySecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.cors();
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
@@ -51,5 +57,31 @@ public class MySecurityConfiguration extends WebSecurityConfigurerAdapter {
 
         http.addFilterBefore(
             new MyAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList(
+            "GET",
+            "POST",
+            "PUT",
+            "PATCH",
+            "DELETE",
+            "OPTIONS"
+        ));
+
+        configuration.setAllowedHeaders(Arrays.asList(
+            "authorization",
+            "content-type",
+            "x-auth-token"
+        ));
+
+        configuration.setExposedHeaders(Arrays.asList("x-auth-token"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
     }
 }
